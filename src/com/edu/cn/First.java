4bdf2d9ef315aecd2e8f7d2d54a8410fd129a27f
tulala <744714162@qq.com>
2021-05-18 10:43:56 +0800

    首次提交323

--- src/com/edu/cn/First.java
+++ src/com/edu/cn/First.java
@@ -3,7 +3,7 @@ package com.edu.cn;
 public class First {
 
     public static void main(String[] args) {
-        System.out.println("测试语句 更新diam123ante");
+        System.out.println("测试语句 更新diam123ant978978987e");
     }
 
 }
