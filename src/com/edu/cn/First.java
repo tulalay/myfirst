e035c0e86b19e5d0eeeafe8beeb3b43bce597df9
tulala <744714162@qq.com>
2021-05-18 11:20:27 +0800

    首次提交6666666666666

--- src/com/edu/cn/First.java
+++ src/com/edu/cn/First.java
@@ -3,7 +3,7 @@ package com.edu.cn;
 public class First {
 
     public static void main(String[] args) {
-        System.out.println("测试语句 更新diam123ant978978987e");
+        System.out.println("测试语句 更新d66666666666iam123ant978978987e");
     }
 
 }
