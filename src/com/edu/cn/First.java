33b3ba37bbea8d0bf9a5e4f2a6d2e3a543fd2064
tulala <744714162@qq.com>
2021-05-18 10:03:49 +0800

    首次提交2

--- src/com/edu/cn/First.java
+++ src/com/edu/cn/First.java
@@ -3,7 +3,7 @@ package com.edu.cn;
 public class First {
 
     public static void main(String[] args) {
-        System.out.println("测试语句");
+        System.out.println("测试语句 更新diamante");
     }
 
 }
