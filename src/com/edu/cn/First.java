a9d8de713af49e411c34708cb9436906f3ceac9f
tulala <744714162@qq.com>
2021-05-30 21:37:13 +0800

    5月30日修改

--- src/com/edu/cn/First.java
+++ src/com/edu/cn/First.java
@@ -3,7 +3,7 @@ package com.edu.cn;
 public class First {
 
     public static void main(String[] args) {
-        System.out.println("测试语句 更新dia3333333333333333333m123ant978978987e");
+        System.out.println("测试语句 更新dia3333333333333jklkjlkj333333m123ant978978987e");
     }
 
 }
